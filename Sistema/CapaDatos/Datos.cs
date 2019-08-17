8cddf31b5834c21f1e7dafcb5db9e7879c3b1740
Prisionero <jozue.guevara.123@hotmail.com>
2019-08-16 21:37:07 -0500

    proyecto agregado

---
+++ Sistema/CapaDatos/Datos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CapaEntidad;
+namespace CapaDatos
+{
+    public class Datos
+    {
+        public int login(Entidad obj)
+        {
+            {
+                int n;
+                string user, pass;
+                user =  obj.usuario;
+                pass = obj.pasword;
+                if (user == "PRISIONERO")
+                {
+                    if (pass == "computron")
+                    {
+                        n = 1;  
+                    }
+                    else
+                    {
+                        n = 2;  
+                    }
+                }
+                else
+                {
+                    n = 2;
+                }
+                return n;
+
+            }
+        }
+
+     }
+}
---
+++ Sistema/CapaNegocio/Negocio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CapaEntidad;
+using CapaDatos;
+namespace CapaNegocio
+{
+    public class Negocio
+    {
+        Datos dts = new Datos();
+
+        public int N_login(Entidad obj)
+        {
+            return dts.login(obj);
+        }
+
+    }
+        
+    
+}
